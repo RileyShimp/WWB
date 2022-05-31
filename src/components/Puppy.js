963a901acd98af93c857eaca81edfa9970779677
Riley Shimp <rilryshimp@gmail.com>
2022-05-30 17:17:29 -0700

    Add Acorn puppy card

--- src/components/Puppy.js
+++ src/components/Puppy.js
@@ -1,13 +1,15 @@
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import image from '../assets/img/FrenchiePuppy1.jpg'
 
 export default function Puppy() {
   return (
     <div className='puppy'>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src='holder.js/100px180' />
+        <Card.Img variant='top' src={image} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Acorn</Card.Title>
+          <Card.Subtitle className='mb-2 text-muted'>Female</Card.Subtitle>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of the card's
             content.
