b6d02d56140e5170f9b19bcbd473e7860fb8a70b
Riley Shimp <rilryshimp@gmail.com>
2022-05-31 12:15:29 -0700

    Refactor puppy page to map over puppyData

--- src/components/Puppy.js
+++ src/components/Puppy.js
@@ -1,22 +1,79 @@
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import image from '../assets/img/FrenchiePuppy1.jpg'
 
 export default function Puppy() {
+  let puppyData = [
+    {
+      name: 'Acorn',
+      sex: 'Male',
+      color: 'Blue',
+      price: '$6500',
+    },
+    {
+      name: 'Bella',
+      sex: 'Female',
+      color: 'Platinum',
+      price: '$8500',
+    },
+    {
+      name: 'Bubba',
+      sex: 'Male',
+      color: 'brown',
+      price: '$4500',
+    },
+    {
+      name: 'Max',
+      sex: 'Male',
+      color: 'brown',
+      price: '$5500',
+    },
+    {
+      name: 'Poncho',
+      sex: 'Male',
+      color: 'blue',
+      price: '$7500',
+    },
+    {
+      name: 'Peanut',
+      sex: 'white tri',
+      color: 'brown',
+      price: '$4500',
+    },
+    {
+      name: 'Nala',
+      sex: 'Female',
+      color: 'black',
+      price: '$5500',
+    }
+  ]
+
   return (
     <div className='puppy'>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={image} />
-        <Card.Body>
-          <Card.Title>Acorn</Card.Title>
-          <Card.Subtitle className='mb-2 text-muted'>Female</Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </Card.Text>
-          <Button variant='primary'>Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Container className='puppyContainer'>
+        <Row>
+          {puppyData.map((puppy) => (
+            <Col sm>
+              <Card className='puppyCard' style={{ width: '15rem' }}>
+                <Card.Img variant='top' src={image} />
+                <Card.Body>
+                  <Card.Title>{puppy.name}</Card.Title>
+                  <Card.Subtitle className='mb-2 text-muted'>{puppy.sex}</Card.Subtitle>
+                  <Card.Text>
+                    {puppy.name} is a {puppy.color} frenchie! Some quick example text where a short
+                    description of the puppy could go.
+                  </Card.Text>
+                  <Card.Subtitle className='mb-2 text-muted'>Price: {puppy.price}</Card.Subtitle>
+                  <Button variant='primary'>Email for purchase info</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   )
 }
