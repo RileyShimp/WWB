76ca0d51b2935814d108a7a0140e215ed3e24b30
Riley Shimp <rilryshimp@gmail.com>
2022-06-04 16:42:58 -0700

    Add Carousel for Homepage header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,9 @@
+import CarouselHeader from "../components/CarouselHeader";
+
 export default function Home (){
-  return <h1>Home</h1>
+  return(
+    <div>
+    <CarouselHeader/>
+    </div>
+  )
 }
