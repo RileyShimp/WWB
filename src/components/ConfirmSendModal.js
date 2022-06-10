88904e55aa54503ffa7d2cf5b504cf675feb2cc6
RileyShimp <rilryshimp@gmail.com>
2022-06-10 10:04:02 -0700

    Add modal to confirm email sent

---
+++ src/components/ConfirmSendModal.js
@@ -0,0 +1,24 @@
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+
+export default function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Email Sent!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Thank you for your interest! We will get back to you as soon as possible!</Modal.Body>
+      </Modal>
+    </>
+  );
+}
