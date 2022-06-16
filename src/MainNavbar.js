b6115e9e6ad0a75a6a293e357e73c5cd2fc70a81
RileyShimp <rilryshimp@gmail.com>
2022-06-15 22:59:17 -0700

    Refactor Navbar to collapse on mobile screens

--- src/MainNavbar.js
+++ src/MainNavbar.js
@@ -5,22 +5,25 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function MainNavbar() {
   return (
-    <Navbar bg='dark' variant='dark'>
+    <Navbar collapseOnSelect expand='sm' bg='dark' variant='dark'>
       <Container>
         <LinkContainer to='/'>
           <Navbar.Brand>Worldwide Bullies</Navbar.Brand>
         </LinkContainer>
-        <Nav className='me-auto'>
-          <LinkContainer to='/puppies'>
-            <Nav.Link>Puppies</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/studs'>
-            <Nav.Link>Studs</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/contact'>
-            <Nav.Link>Contact</Nav.Link>
-          </LinkContainer>
-        </Nav>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <LinkContainer to='/puppies'>
+              <Nav.Link>Puppies</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/studs'>
+              <Nav.Link>Studs</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/contact'>
+              <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
--- src/components/CarouselHeader.js
+++ src/components/CarouselHeader.js
@@ -7,19 +7,21 @@ export default function CarouselHeader() {
   return (
     <div className='Carousel'>
       <Carousel style={{ color: '#14213d', margin: '0 auto' }}>
-        <Carousel.Item>
-          <img className='d-block w-100' src={image1} alt='First slide' />
-          <Carousel.Caption
-            style={{
-              fontWeight: '700',
-              fontSize: '4vw',
-              color: '#8ecae6',
-              textShadow: '2px 2px #023047',
-            }}>
-            <h3 style={{ fontWeight: '700', fontSize: '7vw' }}>Wordwide Bullies</h3>
-            <p>AKC registered puppies and studs</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+              <Carousel.Item>
+                <img className='d-block w-100' src={image2} alt='Third slide' />
+      
+                <Carousel.Caption
+                  style={{
+                    top: '0',
+                    fontWeight: '700',
+                    fontSize: '4vw',
+                    color: '#8ecae6',
+                    textShadow: '2px 2px #023047',
+                  }}>
+                  <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>We Got Studs!</h3>
+                  <p>a variety of studs available</p>
+                </Carousel.Caption>
+              </Carousel.Item>
         <Carousel.Item>
           <img className='d-block w-100' src={image3} alt='Second slide' />
 
@@ -35,21 +37,19 @@ export default function CarouselHeader() {
             <p>Contact us if you interested in adopting! </p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img className='d-block w-100' src={image2} alt='Third slide' />
-
-          <Carousel.Caption
-            style={{
-              top: '0',
-              fontWeight: '700',
-              fontSize: '4vw',
-              color: '#8ecae6',
-              textShadow: '2px 2px #023047',
-            }}>
-            <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>We Got Studs!</h3>
-            <p>a variety of studs available</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+            <Carousel.Item>
+              <img className='d-block w-100' src={image1} alt='First slide' />
+              <Carousel.Caption
+                style={{
+                  fontWeight: '700',
+                  fontSize: '4vw',
+                  color: '#8ecae6',
+                  textShadow: '2px 2px #023047',
+                }}>
+                <h3 style={{ fontWeight: '700', fontSize: '7vw' }}>Wordwide Bullies</h3>
+                <p>AKC registered puppies and studs</p>
+              </Carousel.Caption>
+            </Carousel.Item>
       </Carousel>
     </div>
   )
