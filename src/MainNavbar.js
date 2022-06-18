54ee0697b5d9e88881665bf7dae713d240c4b3ae
RileyShimp <rilryshimp@gmail.com>
2022-06-18 10:45:38 -0700

    Add logo to navbar

--- src/MainNavbar.js
+++ src/MainNavbar.js
@@ -2,29 +2,30 @@ import { LinkContainer } from 'react-router-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import NavLogo from './assets/img/NavLogo1.png'
 
 export default function MainNavbar() {
   return (
-    <Navbar collapseOnSelect expand='sm' bg='dark' variant='dark'>
-      <Container>
-        <LinkContainer to='/'>
-          <Navbar.Brand>Worldwide Bullies</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='me-auto'>
-            <LinkContainer to='/puppies'>
-              <Nav.Link>Puppies</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/studs'>
-              <Nav.Link>Studs</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/contact'>
-              <Nav.Link>Contact</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar collapseOnSelect expand='sm' bg='dark' variant='dark'>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand><img alt='' src={NavLogo}  width='140vw' className='d-inline-block align-top' /></Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>
+              <LinkContainer to='/puppies'>
+                <Nav.Link>Puppies</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/studs'>
+                <Nav.Link>Studs</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>Contact</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import CarouselHeader from '../components/CarouselHeader'
 import Contact from './Contact'
 import Puppies from './Puppies'
 import Studs from './Studs'
-import WWBLogo from '../assets/img/IMG_7218.PNG'
 
 export default function Home() {
   return (
